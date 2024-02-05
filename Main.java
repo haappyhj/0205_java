f2d9deebbec3858bd49f014a1763918d8da54485
haappyhj <149385733+haappyhj@users.noreply.github.com>
2024-02-05 10:47:04 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main{
+    public static void main(String[]args){
+        int x,y,z; 
+        x=y=z=50; // int x=y=z=50; 이렇게 선언하면 안됨! 분리해서 선언하기기
+        System.out.println(x+y+z);
+    }
+}
+
